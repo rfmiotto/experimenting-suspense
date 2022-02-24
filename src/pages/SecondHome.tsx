7f3e49793084448042ee03f242cf91b4e6576c47
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2022-02-24 10:57:20 -0300

    2. refactor suspend async call

--- src/pages/SecondHome.tsx
+++ src/pages/SecondHome.tsx
@@ -15,25 +15,15 @@ import { Spinner } from "../components/Spinner";
 
 const auth = getAuth(firebaseApp);
 
-// So here is our solution: we now have our onAuthStateChanged code inside the
-// promise and we resolve it in the onAuthStateChanged callback function.
-// In order to be good citizens, let's get the unsubscribe function that the
-// onAuthStateChanged function returns (you can name the variable as you want)
-// and execute it after resolving the promise. We do this so the onAuthStateChanged
-// code does not keep running over and over again (since it is an event listener).
-// Despite this solution works, it is not a very scalable or composable architecture.
-// Fortunately, there is a library called "suspend-react" that helps us to write
-// a better code.
-// OBS: if you are trying to run this code, it may not work as expected because,
-// as we will see later on, we lost the ability to have our app update over time
-// with this approach.
-let user: User | null;
+// Instead of using this `let user: User | null;` variable in the module scope,
+// suspend-react actually returns whatever value our promise resolves with. So,
+// if we just pass in the `firebaseUser` to `resolve` and get rid of this `let user`,
+// we can say: `const user = suspend(getInitialAuthState, ["initialAuthState"]);`
 async function getInitialAuthState() {
-  return new Promise<void>((resolve) => {
+  return new Promise<User | null>((resolve) => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-      user = firebaseUser ?? ({ email: "Need to login" } as User);
+      resolve(firebaseUser);
       unsubscribe();
-      resolve();
     });
   });
 }
@@ -47,7 +37,38 @@ function Home() {
   // used to keep track of these different promises in a global cache. For our
   // purposes, we can use the name "initialAuthState" as a key and that will be
   // enough to identify this promise for us.
-  suspend(getInitialAuthState, ["initialAuthState"]);
+  const user = suspend(getInitialAuthState, ["initialAuthState"]);
+
+  // Notice that we managed to use Suspense and remove all the async code from
+  // this component Home. We have hidden all all the third-party async code outside
+  // of our component's rendering cycle up here in this `getInitialAuthState`
+  // function. All we did was to wrap a Promise and resolve it inside of a callback.
+  // Now, Home component is super slim and synchronous. When React hits the
+  // `suspend` call above, since the getInitialAuthState is an appending state,
+  // it is going to stop execution. React is gonna wait for that promise to
+  // resolve and then try to render the component again. The second time it
+  // renders, the `suspend` call is going to resolve synchronously with the value
+  // of our user. React will be able to pass this line and render the code below.
+  // Now we don't have a loading state in the component anymore and we are able
+  // to handle it in our parent component, allowing us to have a suspense boundary
+  // in our tree in case we have more components and want them to behave in a
+  // specific manner.
+  // Also notice that Firebase API doesn’t return a promise! So our async function
+  // needs to return the Firebase call wrapped in a promise, but that one would
+  // resolve immediately. To make it possible to resolve it immediately, we need
+  // to make our Promise such that we can manually call resolve() within the
+  //  Firebase callback.
+
+  // The objective of this lesson is to understand the power of promises with
+  // Suspense. Now, we can turn any async code or async work that needs to be
+  // done into an async function that can then be used to suspend your react
+  // component.
+
+  // So far with our solution, you might have noticed that we lost something
+  // from our original implementation with useEffect. If we change the state
+  // of the user (for example, signout), our UI doesn't update. If we refresh,
+  // we do see the correct UI. We are going to cover this issue in the next
+  // commit.
 
   async function handleSignIn(): Promise<void> {
     await signInWithEmailAndPassword(auth, "test@email.com", "123456");
