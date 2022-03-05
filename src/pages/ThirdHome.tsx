10dcc692bc5e98128889719ad0aac31f691c5f28
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2022-03-05 09:17:05 -0300

    3. presenting the problem

--- src/pages/ThirdHome.tsx
+++ src/pages/ThirdHome.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // In this example, we are going to look at a lower level API from React that
 // will help us understand how to high level APIs actually work. Here, we have
@@ -10,15 +10,32 @@ import { useState } from "react";
 // In React 18, though, this warning is not present anymore, but, if we add a
 // console.log inside our setInterval, we will see that the interval is still
 // going when showing is false. This problem occurs because it is a side effect.
-// To fix this, we have to move the code inside a useEffect.
+// To fix this, we have to move the code inside a useEffect. With this, the
+// setInterval is activated when the component mounts and deactivated when it
+// unmounts.
+
+// Okay, our clock seems to be working fine. But now, let's create another
+// TimestampComponent. If we start toggling this on and off, we end up getting
+// different versions of the time. There is a lot of flakiness coming from the
+// fact that each component has it's own version of the time (i.e., its own
+// useState). This is an example of data that is more global in nature. The
+// current time, from the perspective of React app, should really resolve to the
+// same thing, regardless of which component is referencing it. That is the
+// motivating case for what is called "an external store". So, it would be nice
+// if we could move the time data and the setInterval outside of React, in module
+// scope.
 
 function TimestampComponent() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  setInterval(() => {
-    console.log("setInterval is running even when showing is false");
-    setTime(new Date().toLocaleTimeString());
-  }, 1000);
+  useEffect(() => {
+    const id = setInterval(() => {
+      console.log("setInterval is running even when showing is false");
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
 
   return <p>The time is: {time}</p>;
 }
@@ -27,7 +44,7 @@ function ThirdApp() {
   const [showing, setShowing] = useState(true);
 
   return (
-    <>
+    <div className="flex flex-col">
       <button
         type="button"
         onClick={() => setShowing(!showing)}
@@ -36,7 +53,8 @@ function ThirdApp() {
         Toggle
       </button>
       {showing && <TimestampComponent />}
-    </>
+      <TimestampComponent />
+    </div>
   );
 }
 
